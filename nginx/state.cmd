6c269682d4d41a07f78dae01a06c2bfc0b0f3582
sorgom <110917257+sorgom@users.noreply.github.com>
2026-07-17 17:28:13 +0200

    delete for submodule

--- nginx/state.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-for %%i in (nginx.exe php-cgi.exe) do (
-    echo.
-    echo ===== %%i
-    tasklist /FI "IMAGENAME eq %%i"
-)
--- nginx/stop.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-for %%i in (nginx php-cgi) do taskkill /F /IM %%i.exe >NUL 2>&1
-
-
